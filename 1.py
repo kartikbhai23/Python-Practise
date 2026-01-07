de02bf55a7f4d6273da05b41a7e7c0f717de14fc
Kartik Pandey <dcp1234a@gmail.com>
2026-01-07 21:15:46 +0530

    update answer

--- 1.py
+++
@@ -1,5 +0,0 @@
-#1. Write a program that asks the user for their name and age, then prints a  sentence like:
-a = input("Enter youare name: ")
-b = int(input("enter youare age: "))
-
-print(f"hello {a}, you are {b} year old")
--- Day 3 (Assignment)/09.py
+++ Day 3 (Assignment)/09.py
@@ -11,3 +11,11 @@ non-prime & we'll return false for it.
 For number we'll check in range (2, 6) & it won't get divided by any. So 
 is prime & we'll return true for it. ]
 """
+input_number = int(input("Enter a number to check if it is prime: "))
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
